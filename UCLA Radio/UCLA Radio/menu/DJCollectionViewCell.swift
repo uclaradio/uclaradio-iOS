4d7b124c7ac963e495f9c0dd9162c9a06252426a
ChrisLaganiere <ChrisLaganiere@gmail.com>
2016-06-06 19:49:33 -0700

    fix image rounding with kvo

--- UCLA Radio/UCLA Radio/menu/DJCollectionViewCell.swift
+++ UCLA Radio/UCLA Radio/menu/DJCollectionViewCell.swift
@@ -18,6 +18,8 @@ class DJCollectionViewCell: UICollectionViewCell {
     let imageView = UIImageView()
     let nameLabel = UILabel()
     
+    let ImageChangeContext:UnsafeMutablePointer<()> = nil
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
@@ -25,12 +27,21 @@ class DJCollectionViewCell: UICollectionViewCell {
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .ScaleAspectFill
         imageView.clipsToBounds = true
+        imageView.layer.addObserver(self, forKeyPath: "bounds", options: .New, context: ImageChangeContext)
+        
+        
         contentView.addSubview(nameLabel)
         nameLabel.translatesAutoresizingMaskIntoConstraints = false
         nameLabel.font = UIFont.systemFontOfSize(textFontSize)
         nameLabel.textAlignment = .Center
         
         contentView.addConstraints(preferredConstraints())
+        contentView.layer.cornerRadius = 5
+        contentView.clipsToBounds = true
+    }
+    
+    deinit {
+        imageView.layer.removeObserver(self, forKeyPath: "bounds", context: ImageChangeContext)
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -45,28 +56,36 @@ class DJCollectionViewCell: UICollectionViewCell {
             nameLabel.text = ""
         }
         
+        let placeholder = UIImage(named: "bear")
         if let picture = dj.picture {
-            imageView.sd_setImageWithURL(RadioAPI.absoluteURL(picture))
+            imageView.sd_setImageWithURL(RadioAPI.absoluteURL(picture), placeholderImage: placeholder)
         }
         else {
-            imageView.image = nil
+            imageView.image = placeholder
         }
-    }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        imageView.layer.cornerRadius = imageView.frame.size.height/2.0
     }
     
     // MARK: - Layout
     
     func preferredConstraints() -> [NSLayoutConstraint] {
         var constraints: [NSLayoutConstraint] = []
-        constraints += NSLayoutConstraint.constraintsWithVisualFormat("V:|-[image]-[name(label)]-|", options: [], metrics: ["label": labelHeight], views: ["image": imageView, "name": nameLabel])
+        constraints += NSLayoutConstraint.constraintsWithVisualFormat("V:|-(15)-[image]-[name(label)]-|", options: [], metrics: ["label": labelHeight], views: ["image": imageView, "name": nameLabel])
         constraints += NSLayoutConstraint.constraintsWithVisualFormat("H:|-[name]-|", options: [], metrics: nil, views: ["image": imageView, "name": nameLabel])
         constraints.append(NSLayoutConstraint(item: imageView, attribute: .Width, relatedBy: .Equal, toItem: imageView, attribute: .Height, multiplier: 1.0, constant: 0.0))
         constraints.append(NSLayoutConstraint(item: imageView, attribute: .CenterX, relatedBy: .Equal, toItem: contentView, attribute: .CenterX, multiplier: 1.0, constant: 0.0))
         return constraints
+    }
+    
+    // MARK: - KVO
+    
+    override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
+        if (context == ImageChangeContext) {
+//            print("image changed: \(imageView.frame)")
+            imageView.layer.cornerRadius = imageView.frame.size.height/2.0
+        }
+        else {
+            super.observeValueForKeyPath(keyPath, ofObject: object, change: change, context: context)
+        }
     }
     
 }
