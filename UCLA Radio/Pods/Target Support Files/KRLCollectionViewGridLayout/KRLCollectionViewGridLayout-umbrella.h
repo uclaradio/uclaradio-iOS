42166376606e5faa62e10ed4fb9d19917f4eb232
Nathan Smith <nathan@nathansmith.io>
2017-11-07 10:22:43 -0800

    Update folder structure (#27)
    * update .gitignore
    
    * add macos
    
    * Move to root directory

--- UCLA Radio/Pods/Target Support Files/KRLCollectionViewGridLayout/KRLCollectionViewGridLayout-umbrella.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "KRLCollectionViewGridLayout.h"
-
-FOUNDATION_EXPORT double KRLCollectionViewGridLayoutVersionNumber;
-FOUNDATION_EXPORT const unsigned char KRLCollectionViewGridLayoutVersionString[];
-
