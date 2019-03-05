0cabfc0d7e4a28756d5a4d20ff353cc544fafe98
Joseph Clegg <josephclegg@josephs-macbook-pro-2.local>
2019-03-04 16:25:59 -0800

    removed further unnecessary comments

--- UCLA Radio/custom elements/BaseViewController.swift
+++ UCLA Radio/custom elements/BaseViewController.swift
@@ -12,10 +12,6 @@ class BaseViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-//        if let navigationController = navigationController {
-//            navigationController.navigationBar.barTintColor = UIColor(hex: 0x80333333)
-//        }
         
         //make the background of any BaseViewController transparent
         self.view.backgroundColor = UIColor(white: 1, alpha: 0.0)
@@ -27,11 +23,6 @@ class BaseViewController: UIViewController {
 class BaseTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //view.backgroundColor = Constants.Colors.lightPink
-//        if let navigationController = navigationController {
-//            navigationController.navigationBar.barTintColor = Constants.Colors.darkBackground
-//        }
         
         UIGraphicsBeginImageContext(self.view.frame.size)
         UIImage(named: "background")?.draw(in: self.view.bounds)
